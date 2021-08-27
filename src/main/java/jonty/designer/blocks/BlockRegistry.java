ede005969ecd221c339714267fe8f73d243047c1
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-08-27 17:31:36 +0100

    clean up registry

--- src/main/java/jonty/designer/blocks/BlockRegistry.java
+++ src/main/java/jonty/designer/blocks/BlockRegistry.java
@@ -3,14 +3,8 @@ package jonty.designer.blocks;
 import io.github.jamalam360.libjam.registry.entry.BlockEntry;
 import jonty.designer.blocks.block.DesignerStairsBlock;
 import jonty.designer.blocks.block.OneWayWindowBlock;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.*;
-import net.minecraft.entity.EntityType;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.BlockView;
 
 /**
  * @author Jamalam360 AND Jomlom but mostly Jamalam obviously
@@ -18,39 +12,28 @@ import net.minecraft.world.BlockView;
 
 @SuppressWarnings("unused")
 public class BlockRegistry {
-    public static final BlockEntry D_GLASS = new BlockEntry("d_glass", new GlassBlock(FabricBlockSettings.copy(Blocks.GLASS).allowsSpawning(BlockRegistry::never).solidBlock(BlockRegistry::never).suffocates(BlockRegistry::never).blockVision(BlockRegistry::never)), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry ONE_WAY_STONE = new BlockEntry("one_way_stone", new OneWayWindowBlock(FabricBlockSettings.copy(Blocks.GLASS).allowsSpawning(BlockRegistry::never).solidBlock(BlockRegistry::never).suffocates(BlockRegistry::never).blockVision(BlockRegistry::never)), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry D_GLASS = new BlockEntry("d_glass", new GlassBlock(DesignerBlockSettings.getGlassSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry ONE_WAY_STONE = new BlockEntry("one_way_stone", new OneWayWindowBlock(DesignerBlockSettings.getGlassSettings()), ItemGroup.BUILDING_BLOCKS);
 
-    public static final BlockEntry D_STONE = new BlockEntry("d_stone", new Block(FabricBlockSettings.of(Material.STONE).strength(3, 7).sounds(BlockSoundGroup.DEEPSLATE_BRICKS).breakByTool(FabricToolTags.PICKAXES).requiresTool()), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry D_SANDSTONE = new BlockEntry("d_sandstone", new Block(FabricBlockSettings.of(Material.STONE).strength(3, 7).sounds(BlockSoundGroup.DEEPSLATE_BRICKS).breakByTool(FabricToolTags.PICKAXES).requiresTool()), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry D_LIMESTONE = new BlockEntry("d_limestone", new Block(FabricBlockSettings.of(Material.STONE).strength(3, 7).sounds(BlockSoundGroup.DEEPSLATE_BRICKS).breakByTool(FabricToolTags.PICKAXES).requiresTool()), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry D_DARKSTONE = new BlockEntry("d_darkstone", new Block(FabricBlockSettings.of(Material.STONE).strength(3, 7).sounds(BlockSoundGroup.DEEPSLATE_BRICKS).breakByTool(FabricToolTags.PICKAXES).requiresTool()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry D_STONE = new BlockEntry("d_stone", new Block(DesignerBlockSettings.getBricksSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry D_SANDSTONE = new BlockEntry("d_sandstone", new Block(DesignerBlockSettings.getBricksSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry D_LIMESTONE = new BlockEntry("d_limestone", new Block(DesignerBlockSettings.getBricksSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry D_DARKSTONE = new BlockEntry("d_darkstone", new Block(DesignerBlockSettings.getBricksSettings()), ItemGroup.BUILDING_BLOCKS);
 
-    public static final BlockEntry LIMESTONE = new BlockEntry("darkstone", new Block(FabricBlockSettings.of(Material.STONE).strength(2, 4).sounds(BlockSoundGroup.DEEPSLATE_BRICKS).breakByTool(FabricToolTags.PICKAXES).requiresTool()), ItemGroup.MATERIALS);
-    public static final BlockEntry DARKSTONE = new BlockEntry("limestone", new Block(FabricBlockSettings.of(Material.STONE).strength(2, 4).sounds(BlockSoundGroup.DEEPSLATE_BRICKS).breakByTool(FabricToolTags.PICKAXES).requiresTool()), ItemGroup.MATERIALS);
+    public static final BlockEntry LIMESTONE = new BlockEntry("darkstone", new Block(DesignerBlockSettings.getStoneSettings()), ItemGroup.MATERIALS);
+    public static final BlockEntry DARKSTONE = new BlockEntry("limestone", new Block(DesignerBlockSettings.getStoneSettings()), ItemGroup.MATERIALS);
 
-    public static final BlockEntry D_WOOL_RED = new BlockEntry("d_wool_red", new Block(FabricBlockSettings.of(Material.WOOL).strength(0.5F, 3).sounds(BlockSoundGroup.WOOL).breakByTool(FabricToolTags.SHEARS)), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry D_WOOL_BLUE = new BlockEntry("d_wool_blue", new Block(FabricBlockSettings.of(Material.WOOL).strength(0.5F, 3).sounds(BlockSoundGroup.WOOL).breakByTool(FabricToolTags.SHEARS)), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry D_WOOL_GREEN = new BlockEntry("d_wool_green", new Block(FabricBlockSettings.of(Material.WOOL).strength(0.5F, 3).sounds(BlockSoundGroup.WOOL).breakByTool(FabricToolTags.SHEARS)), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry D_WOOL_PURPLE = new BlockEntry("d_wool_purple", new Block(FabricBlockSettings.of(Material.WOOL).strength(0.5F, 3).sounds(BlockSoundGroup.WOOL).breakByTool(FabricToolTags.SHEARS)), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry D_WOOL_WHITE = new BlockEntry("d_wool_white", new Block(FabricBlockSettings.of(Material.WOOL).strength(0.5F, 3).sounds(BlockSoundGroup.WOOL).breakByTool(FabricToolTags.SHEARS)), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry D_WOOL_BLACK = new BlockEntry("d_wool_black", new Block(FabricBlockSettings.of(Material.WOOL).strength(0.5F, 3).sounds(BlockSoundGroup.WOOL).breakByTool(FabricToolTags.SHEARS)), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry D_WOOL_RED = new BlockEntry("d_wool_red", new Block(DesignerBlockSettings.getWoolSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry D_WOOL_BLUE = new BlockEntry("d_wool_blue", new Block(DesignerBlockSettings.getWoolSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry D_WOOL_GREEN = new BlockEntry("d_wool_green", new Block(DesignerBlockSettings.getWoolSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry D_WOOL_PURPLE = new BlockEntry("d_wool_purple", new Block(DesignerBlockSettings.getWoolSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry D_WOOL_WHITE = new BlockEntry("d_wool_white", new Block(DesignerBlockSettings.getWoolSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry D_WOOL_BLACK = new BlockEntry("d_wool_black", new Block(DesignerBlockSettings.getWoolSettings()), ItemGroup.BUILDING_BLOCKS);
 
-    public static final BlockEntry CHESTNUT_PLANKS = new BlockEntry("chestnut_planks", new Block(FabricBlockSettings.of(Material.WOOD).strength(2, 5).sounds(BlockSoundGroup.WOOD).breakByTool(FabricToolTags.AXES)), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry CHESTNUT_WOOD = new BlockEntry("chestnut_wood", new Block(FabricBlockSettings.of(Material.WOOD).strength(2, 4).sounds(BlockSoundGroup.WOOD).breakByTool(FabricToolTags.AXES)), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry CHESTNUT_LOG = new BlockEntry("chestnut_log", new Block(FabricBlockSettings.of(Material.WOOD).strength(2, 4).sounds(BlockSoundGroup.WOOD).breakByTool(FabricToolTags.AXES)), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry CHESTNUT_LEAVES = new BlockEntry("chestnut_leaves", new LeavesBlock(FabricBlockSettings.copy(Blocks.OAK_LEAVES).allowsSpawning(BlockRegistry::never).solidBlock(BlockRegistry::never).suffocates(BlockRegistry::never).blockVision(BlockRegistry::never)), ItemGroup.DECORATIONS);
-    public static final BlockEntry CHESTNUT_SLAB = new BlockEntry("chestnut_slab", new SlabBlock(FabricBlockSettings.copy(Blocks.OAK_SLAB).allowsSpawning(BlockRegistry::never).solidBlock(BlockRegistry::never).suffocates(BlockRegistry::never).blockVision(BlockRegistry::never)), ItemGroup.BUILDING_BLOCKS);
-    public static final BlockEntry CHESTNUT_STAIRS = new BlockEntry("chestnut_stairs", new DesignerStairsBlock(CHESTNUT_PLANKS.getBlock().getDefaultState(), FabricBlockSettings.copy(Blocks.OAK_STAIRS).allowsSpawning(BlockRegistry::never).solidBlock(BlockRegistry::never).suffocates(BlockRegistry::never).blockVision(BlockRegistry::never)), ItemGroup.BUILDING_BLOCKS);
-
-
-    @SuppressWarnings("unused")
-    public static boolean never(BlockState state, BlockView world, BlockPos pos) {
-        return false;
-    }
-
-    @SuppressWarnings("unused")
-    public static Boolean never(BlockState state, BlockView world, BlockPos pos, EntityType<?> type) {
-        return false;
-    }
+    public static final BlockEntry CHESTNUT_PLANKS = new BlockEntry("chestnut_planks", new Block(DesignerBlockSettings.getPlanksSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry CHESTNUT_WOOD = new BlockEntry("chestnut_wood", new Block(DesignerBlockSettings.getWoodSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry CHESTNUT_LOG = new BlockEntry("chestnut_log", new PillarBlock(DesignerBlockSettings.getWoodSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry CHESTNUT_LEAVES = new BlockEntry("chestnut_leaves", new LeavesBlock(DesignerBlockSettings.getLeavesSettings()), ItemGroup.DECORATIONS);
+    public static final BlockEntry CHESTNUT_SLAB = new BlockEntry("chestnut_slab", new SlabBlock(DesignerBlockSettings.getPlanksSettings()), ItemGroup.BUILDING_BLOCKS);
+    public static final BlockEntry CHESTNUT_STAIRS = new BlockEntry("chestnut_stairs", new DesignerStairsBlock(CHESTNUT_PLANKS.getBlock().getDefaultState(), DesignerBlockSettings.getPlanksSettings()), ItemGroup.BUILDING_BLOCKS);
 }
--- src/main/java/jonty/designer/blocks/DesignerBlockSettings.java
+++ src/main/java/jonty/designer/blocks/DesignerBlockSettings.java
@@ -1,7 +1,56 @@
 package jonty.designer.blocks;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.Material;
+import net.minecraft.entity.EntityType;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+
 /**
  * @author Jamalam360
  */
 public class DesignerBlockSettings {
+    public static FabricBlockSettings getWoolSettings() {
+        return FabricBlockSettings.of(Material.WOOL).strength(0.8F).sounds(BlockSoundGroup.WOOL);
+    }
+
+    public static FabricBlockSettings getBricksSettings() {
+        return FabricBlockSettings.of(Material.STONE).strength(3, 7).sounds(BlockSoundGroup.DEEPSLATE_BRICKS).breakByTool(FabricToolTags.PICKAXES).requiresTool();
+    }
+
+    public static FabricBlockSettings getStoneSettings() {
+        return FabricBlockSettings.of(Material.STONE).strength(2, 4).sounds(BlockSoundGroup.DEEPSLATE_BRICKS).breakByTool(FabricToolTags.PICKAXES).requiresTool();
+    }
+
+    public static FabricBlockSettings getGlassSettings() {
+        return FabricBlockSettings.of(Material.GLASS).strength(0.3F).sounds(BlockSoundGroup.GLASS).nonOpaque().allowsSpawning(DesignerBlockSettings::never).solidBlock(DesignerBlockSettings::never).suffocates(DesignerBlockSettings::never).blockVision(DesignerBlockSettings::never);
+    }
+
+    public static FabricBlockSettings getWoodSettings() {
+        return FabricBlockSettings.of(Material.WOOD).strength(2, 4).sounds(BlockSoundGroup.WOOD).breakByTool(FabricToolTags.AXES);
+    }
+
+    public static FabricBlockSettings getLeavesSettings() {
+        return FabricBlockSettings.of(Material.LEAVES).strength(0.2F).ticksRandomly().sounds(BlockSoundGroup.GRASS).nonOpaque().allowsSpawning(DesignerBlockSettings::canSpawnOnLeaves).suffocates(DesignerBlockSettings::never).blockVision(DesignerBlockSettings::never);
+    }
+
+    public static FabricBlockSettings getPlanksSettings() {
+        return FabricBlockSettings.of(Material.WOOD, MapColor.OAK_TAN).strength(2.0F, 3.0F).sounds(BlockSoundGroup.WOOD).breakByTool(FabricToolTags.AXES);
+    }
+
+    public static boolean never(BlockState state, BlockView world, BlockPos pos) {
+        return false;
+    }
+
+    public static Boolean never(BlockState state, BlockView world, BlockPos pos, EntityType<?> type) {
+        return false;
+    }
+
+    private static Boolean canSpawnOnLeaves(BlockState state, BlockView world, BlockPos pos, EntityType<?> type) {
+        return type == EntityType.OCELOT || type == EntityType.PARROT;
+    }
 }
